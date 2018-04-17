aaa95462b8df6dacc5ccf9d6b56095f62f2d0ac4
Khalid you <k.ihellioun@ump.ac.ma>
2018-04-17 16:03:15 +0200

    assertTrue

---
+++ Test/DemoTest.java
@@ -0,0 +1,26 @@
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.string.Demo;
+
+class DemoTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() {
+		Demo demo =new Demo();
+		assertTrue(demo.compareStringIgnoreCase("hello", "Hello"));
+	}
+
+}
---
+++ src/com/string/Demo.java
@@ -0,0 +1,11 @@
+ package com.string;
+
+public class Demo {
+	
+	public boolean compareStringIgnoreCase(String first,String second) {
+		if(first.equalsIgnoreCase(second)) {
+			return true;
+		}
+		return false;	
+	}
+}
